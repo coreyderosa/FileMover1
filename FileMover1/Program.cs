728a6bb58c9be5551279f16f8c5479ec5b1a5477
coreyderosa <coreyderosa@gmail.com>
2016-12-05 20:19:42 -0700

    Move files using Modified Date
    Successfully moved files based on modified date being within 24 hours
    from present time.

--- FileMover1/Program.cs
+++ FileMover1/Program.cs
@@ -9,35 +9,41 @@ namespace FileMover1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void FileMover()
         {
-            string rootFolderPath = @"D:\Documents\Folder A\";
+            string rootFolderPath = "D:\\Documents\\Folder A\\";
             string destinationPath = "D:\\Documents\\Folder B\\";
             string txtFile = "*.txt";
             string[] sourceFileList = Directory.GetFiles(rootFolderPath, txtFile);
 
             //if (File.GetLastWriteTime(rootFolderPath) && )
             //{
-                //asd
-            
+            //asd
+
             foreach (var file in sourceFileList)
             {
-                //string fileToMove = file;
+                DateTime dt = File.GetLastWriteTime(file); //gets file's most recent modified time
+                Console.WriteLine(file + " modified time: " + dt);
+                Console.Read();
                 Console.WriteLine("Moving " + file);
                 string moveTo = destinationPath;
                 Console.WriteLine("New location " + moveTo);
                 Console.Read();
-                string combine = Path.Combine(moveTo, Path.GetFileName(file));
+                string combine = Path.Combine(moveTo, Path.GetFileName(file)); //combines the dest folder and source file name
                 Console.WriteLine("Combined path: " + combine);
                 Console.Read();
-                //string fileName = Path.GetFileName(file);
-                //Console.WriteLine("Filename: " + fileName);
-                //Console.Read();
-                //moving file
-                //Directory.Move(fileToMove, moveTo);
-                File.Move(file, combine);
+
+                if (Math.Abs((File.GetLastWriteTime(file) - DateTime.Today.Date).TotalHours) < 24)
+                {
+                    File.Move(file, combine);
+                }
             }
             //}
+        }
+
+        static void Main(string[] args)
+        {
+            FileMover();
         }
     }
 }
